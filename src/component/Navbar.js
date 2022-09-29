5fd76c2a7744312157551b1e8bbebe43a2b63534
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-09-29 09:05:53 +0700

    Feat:do resume

---
+++ src/component/Navbar.js
@@ -0,0 +1,8 @@
+function Navbar() {
+  return (
+    <div className="navbar flex justify-center flex-row border-2 border-red-400">
+      <h1>This is navbar</h1>
+    </div>
+  );
+}
+export default Navbar;
